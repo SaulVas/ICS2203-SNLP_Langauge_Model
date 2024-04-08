1e5e5184f9a7ab99ae2e5518494b5ab27ab1357d
SaulVas <saul.vassallo.22@um.edu.mt>
2024-04-09 00:17:09 +0200

    only generates data sets if not already present

--- src/main.py
+++ src/main.py
@@ -26,23 +26,33 @@ for number_of_words in range(1, 4):
     with open(f'n_grams/corpus/{number_of_words}_gram_counts.json', 'w', encoding='utf-8') as fp:
         json.dump(n_gram_counts, fp, indent=4)
 
-# Splitting the corpus into train, validation, and test sets
-train = []
-validation = []
-test = []
+TRAIN_FILE_PATH = '../data/training_set.xml'
+VALIDATION_FILE_PATH = '../data/VALIDATION_set.xml'
+TEST_FILE_PATH = '../data/test_set.xml'
+if not (os.path.exists(TRAIN_FILE_PATH)
+        and os.path.exists(VALIDATION_FILE_PATH)
+        and os.path.exists(TEST_FILE_PATH)):
+# Splitting the corpus into train, validation, and test sets if not already created
+    train = []
+    validation = []
+    test = []
 
+    for directory in directories:
+        dir_path = os.path.join(BASE_PATH, directory)
+        for file in os.listdir(dir_path):
+            if file.endswith('.xml'):
+                file_path = os.path.join(dir_path, file)
+                tree = ET.parse(file_path)
+                root = tree.getroot()
+                sentences = list(root.findall('.//s'))
+                split_and_append_elements(sentences, train, validation, test)
 
-
-for directory in directories:
-    dir_path = os.path.join(BASE_PATH, directory)
-    for file in os.listdir(dir_path):
-        if file.endswith('.xml'):
-            file_path = os.path.join(dir_path, file)
-            tree = ET.parse(file_path)
-            root = tree.getroot()
-            sentences = list(root.findall('.//s'))
-            split_and_append_elements(sentences, train, validation, test)
-
-write_xml_from_elements(train, '../data/training_set.xml')
-write_xml_from_elements(validation, '../data/validation_set.xml')
-write_xml_from_elements(test, '../data/test_set.xml')
+    if os.path.exists(TRAIN_FILE_PATH):
+        os.remove(TRAIN_FILE_PATH)
+    if os.path.exists(VALIDATION_FILE_PATH):
+        os.remove(VALIDATION_FILE_PATH)
+    if os.path.exists(TEST_FILE_PATH):
+        os.remove(TEST_FILE_PATH)
+    write_xml_from_elements(train, TRAIN_FILE_PATH)
+    write_xml_from_elements(validation, VALIDATION_FILE_PATH)
+    write_xml_from_elements(test, TEST_FILE_PATH)
